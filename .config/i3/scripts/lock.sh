140733baaa611fdd0f02a930e54c95c7d30dcc15
Twor <i@twor.me>
2022-11-12 22:36:10 +0800

    add picom

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scrot /tmp/screen.png
-ICON="~/.config/i3/imgs/1.png"
+ICON="$HOME/.config/i3/imgs/1.png"
 TMPBG=/tmp/screen.png
 convert $TMPBG -blur 0x5 $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
